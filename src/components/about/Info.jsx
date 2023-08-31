95dd0d97458fc95419e0ca8fc70ded28d287be02
fahad39 <fahadhussain0127@gmail.com>
2023-08-31 16:58:40 +0500

    added about page

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Info() {
   return (
     <div className="about__info grid">
-      <div className="about__box about__icon">
-        <box-icon name="award"></box-icon>
+      <div className="about__box">
+        <box-icon name="award about__icon"></box-icon>
         <h3 className="about__title">Experience</h3>
         <span className="about__subtitle">2 Years Working</span>
       </div>
