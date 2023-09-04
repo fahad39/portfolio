e276b2b73f5675deeef88556d9bf03d6d80bbfc9
fahad39 <fahadhussain0127@gmail.com>
2023-09-04 19:54:00 +0500

    added footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Testimonial from './components/Testimonials/Testimonial';
 import About from './components/about/About';
 import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Qualification from './components/qualification/Qualification';
@@ -20,6 +21,7 @@ function App() {
         <Qualification />
         <Testimonial />
         <Contact />
+        <Footer />
       </main>
     </>
   );
