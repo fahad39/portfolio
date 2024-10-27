d24f996193a2f428bb4eb26a28b9e9ccd619f124
fahad39 <fahadhussain0127@gmail.com>
2024-10-27 12:04:09 +0500

    portfolio update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,7 +8,7 @@ function About() {
   return (
     <section className="about section" id="about">
       <h2 className="section__title">About Me</h2>
-      <span className="section__subtitle">My Introduction</span>
+      <span className="section__subtitle">My Introduction </span>
       <div className="about__container container grid">
         <img src={AboutImg} alt="" className="about__img" />
         <div className="about__data">
