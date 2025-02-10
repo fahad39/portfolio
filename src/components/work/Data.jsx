c5427dbe8145953db138d40565b40f28fd24cde2
fahad39 <fahadhussain0127@gmail.com>
2025-02-10 23:17:22 +0500

    added new project

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -4,45 +4,53 @@ import Work3 from "../../assets/voice_quiz.png";
 import Work4 from "../../assets/audio_app.png";
 import Work5 from "../../assets/gym-app.png";
 import Work6 from "../../assets/3dteebuilder.png";
+import Work7 from "../../assets/work7.png";
 
 export const projectsData = [
   {
     id: 1,
+    image: Work7,
+    title: "Paws Camp",
+    category: "web",
+    link: "https://travel-web-blond-sigma.vercel.app/",
+  },
+  {
+    id: 2,
     image: Work6,
     title: "3D Tee Crafter",
     category: "web",
     link: "https://3d-webapp-ebon.vercel.app/",
   },
   {
-    id: 2,
+    id: 3,
     image: Work1,
     title: "Real Estate Website",
     category: "web",
     link: "https://real-estate-web-alpha.vercel.app/",
   },
   {
-    id: 3,
+    id: 4,
     image: Work5,
     title: "EvoGym Website",
     category: "web",
     link: "https://real-estate-web-alpha.vercel.app/",
   },
   {
-    id: 4,
+    id: 5,
     image: Work2,
     title: "Shoe App Design",
     category: "design",
     link: "https://www.behance.net/gallery/100889717/Shoe-World",
   },
   {
-    id: 5,
+    id: 6,
     image: Work3,
     title: "Voice Quiz App design",
     category: "design",
     link: "https://www.behance.net/gallery/100851587/Voice-Quiz",
   },
   {
-    id: 6,
+    id: 7,
     image: Work4,
     title: "Audible Exploration App design",
     category: "design",
